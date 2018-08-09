b13f801010b3f02c00a0a18d29ac9ee701fd645a
David <davidgcastro93@gmail.com>
2018-08-09 19:57:32 -0400

    updated

--- client/index.js
+++ client/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss';
 ReactDOM.render(
   <div>
     <Main />
