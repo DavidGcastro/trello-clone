b203cfec4890e1f5ec7baf6a4cae5b4f6901cc75
David <davidgcastro93@gmail.com>
2018-10-02 01:01:46 -0400

    created home components

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './Login';
+import Home from './Home';
+import Nav from './Nav';
 
 const Main = () => {
-  return <Login />;
+  return (
+    <Router>
+      <div>
+        <Route path="/" component={Nav} />
+        <Route path="/" component={Home} />
+      </div>
+    </Router>
+  );
 };
 
 export default Main;
