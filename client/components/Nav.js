e883d1477e3f1b247896e2e36eb0998d76799b43
David <davidgcastro93@gmail.com>
2018-10-10 17:12:26 -0400

    added hamburger button functionality

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HamburgerButton } from 'react-hamburger-button';
 
-const Nav = () => {
-  return (
-    <div className="nav">
-      <div className="nav--logo--parent">
-        <img className="nav--logo" src="./assets/images/logo.png" />
-        <h1 className="logo--name">BATON</h1>
-      </div>
-      <div className="nav--innerParent">
-        <Link to="/">
-          <p className="nav--item">Product</p>
-        </Link>
-        <Link to="/">
-          <p className="nav--item">Price</p>
-        </Link>
-        <Link to="/">
-          <p className="nav--item">Contact Sales</p>
-        </Link>
-        <Link to="/">
-          <p className="nav--item">About</p>
-        </Link>
-        <Link to="/">
-          <p className="nav--item">Log In</p>
-        </Link>
-        <Link className="nav--item--button" to="/">
-          <p className="nav--item">Start Free Trial</p>
-        </Link>
-      </div>
-    </div>
-  );
-};
+export default class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+      open: false
+    };
+  }
 
-export default Nav;
+  handleClick() {
+    this.setState({ open: !this.state.open });
+  }
+  render() {
+    return (
+      <div className="nav">
+        <div className="nav--logo--parent">
+          <img className="nav--logo" src="./assets/images/logo.png" />
+          <h1 className="logo--name">BATON</h1>
+        </div>
+        <div className="hamburger--parent">
+          <HamburgerButton
+            open={this.state.open}
+            onClick={this.handleClick.bind(this)}
+            width={'4vh'}
+            height={20}
+            strokeWidth={1}
+            color="black"
+            animationDuration={0.5}
+          />
+        </div>
+      </div>
+    );
+  }
+}
