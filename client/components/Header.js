43d6c64a353d9b6422d986f805eff145c0943eb3
David <davidgcastro93@gmail.com>
2018-10-11 13:05:44 -0400

    adjusted wrapper class

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,27 +2,33 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="home--parent">
-      <div className="home--content padder">
-        <div className="home--textContainer">
-          <span className="text--large">
-            Make more time for the work that matters most
-          </span>
-          <span className="text--reg home--section">
-            Baton helps you coordinate all the work your team does together. So
-            everyone knows what needs to get done, who’s responsible for doing
-            it, and when it’s due.
-          </span>
+    <div className='wrapper'>
+      <div className="home--parent">
+        <div className="home--content padder">
+          <div className="home--textContainer">
+            <span className="text--large">
+              Make more time for the work that matters most
+            </span>
+            <span className="text--reg home--section">
+              Baton helps you coordinate all the work your team does together.
+              So everyone knows what needs to get done, who’s responsible for
+              doing it, and when it’s due.
+            </span>
+          </div>
+          <img className="centered-image" src="./assets/images/waffle.png" />
+          <form className="form--parent">
+            <input
+              className="email-input"
+              type="email"
+              placeholder="name@company.com"
+            />
+            <input
+              className="button--submit"
+              type="submit"
+              value="Start Free Trial"
+            />
+          </form>
         </div>
-        <img className="centered-image" src="./assets/images/waffle.png" />
-        <form className="form--parent">
-          <input
-            className="email-input"
-            type="email"
-            placeholder="name@company.com"
-          />
-          <input className="button--submit" type="submit" value="Start Free Trial" />
-        </form>
       </div>
     </div>
   );
