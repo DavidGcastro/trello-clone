399ca6a50a884ea7ce5c9bd134ffb8343129e192
David <davidgcastro93@gmail.com>
2018-10-11 00:25:16 -0400

    finished callout

--- client/components/Callout.js
+++ client/components/Callout.js
@@ -3,11 +3,20 @@ import React from 'react';
 const Callout = () => {
   return (
     <div className="callout--parent">
-      <span className="text--large">Waffle is loved all over the world</span>
-      <span className="text--reg">
-        Over 100,000+ developers worldwide have made their lives easier by using
-        Waffle
-      </span>
+      <div className="callout--content padder">
+        <span className="text--large">Waffle is loved all over the world</span>
+        <span className="text--reg">
+          Over 100,000+ developers worldwide have made their lives easier by
+          using Waffle
+        </span>
+        <div>
+          <input
+            className="button--action"
+            type="submit"
+            value="Start Free Trial"
+          />
+        </div>
+      </div>
     </div>
   );
 };
