55a160e124023e50fbe036e7e789a5d993691a9b
David <davidgcastro93@gmail.com>
2018-10-11 23:34:21 -0400

    changes

---
+++ client/screens/Home.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Header from '../components/Header';
+import Callout from '../components/Callout';
+
+const Home = () => {
+    return (
+        <div className="home--grandparent">
+            <Header />
+            <Callout />
+        </div>
+    );
+};
+
+export default Home;
