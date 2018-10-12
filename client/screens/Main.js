61610ccbd288f750c07041f90c0d3dfc97101fd7
David <davidgcastro93@gmail.com>
2018-10-12 14:25:43 -0400

    redo

--- client/screens/Main.js
+++ client/screens/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
 import Nav from './Nav';
 
 const Main = () => {
@@ -8,8 +7,6 @@ const Main = () => {
     <Router>
       <div>
         <Nav />
-        <div className="content" />
-        <Home />
       </div>
     </Router>
   );
