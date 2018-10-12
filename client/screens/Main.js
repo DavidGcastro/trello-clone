3b13a53ac6959136851692003f14fafe87ccc8f7
David <davidgcastro93@gmail.com>
2018-10-11 23:04:14 -0400

    changes

--- client/screens/Main.js
+++ client/screens/Main.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Nav from './Nav';
 
-export default class Main extends React.Component {
-  //why callback recommended
-  render() {
-    return (
-      <Router>
-        <div>
-          <Nav />
-          <Home />
-        </div>
-      </Router>
-    );
-  }
-}
+const Main = () => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <div className="content" />
+        <Home />
+      </div>
+    </Router>
+  );
+};
+
+export default Main;
