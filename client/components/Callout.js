46012416e948bce181299315527dc5457e9383c8
David <davidgcastro93@gmail.com>
2018-10-11 23:41:13 -0400

    finished refactor

--- client/components/Callout.js
+++ client/components/Callout.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Callout = () => {
   return (
-    <div className="callout--parent">
-      <div className="callout--content padder spacer">
-        <span className="text--large spacer">Goodie is loved all over the world</span>
+    <div className="callout--parent padder">
+      <div className="callout--content spacer">
+        <span className="text--large spacer">
+          Goodie is loved all over the world
+        </span>
         <span className="text--reg spacer">
           Over 100,000+ developers worldwide have made their lives easier by
           using Goodie
         </span>
-        <div>
-          <input
-            className="button--action"
-            type="submit"
-            value="Start Free Trial"
-          />
-        </div>
       </div>
+      <input
+        className="button--action"
+        type="submit"
+        value="Start Free Trial"
+      />
     </div>
   );
 };
