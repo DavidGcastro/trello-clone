4c4f04d68a80f77605a2ba5c32d3218948957394
David <davidgcastro93@gmail.com>
2018-10-16 20:03:50 -0400

    finished callout adjusted footer, button

--- client/components/Callout.js
+++ client/components/Callout.js
@@ -5,14 +5,25 @@ const Callout = () => {
     <div className="callout wrapper divider">
       <div className="callout--content innerPadding">
         <div className="callout--textParent">
-          <span className="text--large--bold">
-            We create long-lasting, positive growth for your business.
-          </span>
-          <span className="header--description">
-            Spring is a digital studio based in Paris with an itch for
-            behavioural science, busy designing and building meaningful digital
-            products and services for clients worldwide.
-          </span>
+          <div>
+            <span className="text--large--bold">
+              We create long-lasting, positive growth for your business.
+            </span>
+            <span className="header--description">
+              Spring is a digital studio based in Paris with an itch for
+              behavioural science, busy designing and building meaningful
+              digital products and services for clients worldwide.
+            </span>
+          </div>
+          <input
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+            }}
+            value="Try For Free"
+            className="button--action"
+            type="button"
+          />
         </div>
         <img
           className="callout-image"
