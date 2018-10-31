d708c40a6b4f9cab1e0001807f3dd837bc79e0b8
David <davidgcastro93@gmail.com>
2018-10-31 19:29:32 -0400

    before adding passport authentication

--- server/auth/login.js
+++ server/auth/login.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 const { User } = require('../db/models/index');
 const passport = require('passport');
-const decrypt = require('../utils/hashing');
+const decrypt = require('../utils/hashing').isCorrectPassword;
+const LocalStrategy = require('passport-local').Strategy;
+
+// configure passport.js to use the local strategy
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    console.log('Inside local strategy callback');
+    // here is where you make a call to the database
+    // to find the user based on their username or email address
+    User.findOne({
+      where: {
+        email,
+        password
+      }
+    })
+      .then(user => {
+        if (!user) {
+          return done(null, false);
+        } else if (decrypt(password, user.salt(), user.password())) {
+          console.log('Local strategy returned true');
+          return done(null, user);
+        } else {
+          console.log('Password not found');
+          res.sendStatus(404);
+        }
+      })
+      .catch(err => done(err));
+  })
+);
 
 router.post('/', (req, res, next) => {
   let { email, password } = req.body;
@@ -13,9 +41,7 @@ router.post('/', (req, res, next) => {
     .then(user => {
       if (!user) {
         res.sendStatus(401);
-      } else if (
-        decrypt.isCorrectPassword(password, user.salt(), user.password())
-      ) {
+      } else if (decrypt(password, user.salt(), user.password())) {
         console.log('PASSWORD FOUND');
         req.session.userId = user.id;
         req.session.userName = user.firstName;
