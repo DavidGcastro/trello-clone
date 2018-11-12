5935b44fb3c0fbe24c96e673f16fee55d419bf05
David <davidgcastro93@gmail.com>
2018-11-11 23:03:21 -0500

    user projects are set on inital login

--- client/redux/reducers/user.js
+++ client/redux/reducers/user.js
@@ -1,21 +1,33 @@
 import axios from 'axios';
 const SET_USER = 'SET_USER';
 const LOG_OUT = 'LOG_OUT';
+const GET_PROJECTS = 'GET_PROJECTS';
+const GET_SUBTASKS = 'GET_SUBTASKS';
 
 /**
  * ACTION CREATORS
  */
 const setUser = user => ({ type: SET_USER, user });
-
+const getUserProjects = projects => ({ type: GET_PROJECTS, projects });
+const getUserSubtasks = subtasks => ({ type: GET_SUBTASKS, subtasks });
 const logoutUser = () => ({ type: LOG_OUT });
 /**
  *
  * THUNK CREATORS
  */
+
 export const setUserAsync = () => dispatch =>
   axios
     .get('/auth/me')
     .then(me => dispatch(setUser(me.data)))
+    .then(res => {
+      let id = res.user.id;
+      axios
+        .get(`api/users/${id}`)
+        .then(x => dispatch(getUserProjects(x.data[0].projects)));
+      return res;
+    })
+    .then(x => console.log(x.user))
     .catch(err => console.log(err));
 
 export const logOutUserAsync = () => dispatch =>
@@ -33,6 +45,8 @@ export default function(initialState = {}, action) {
       return { ...initialState, user: action.user };
     case LOG_OUT:
       return { ...initialState, user: false };
+    case GET_PROJECTS:
+      return { ...initialState, projects: action.projects };
     default:
       return initialState;
   }
